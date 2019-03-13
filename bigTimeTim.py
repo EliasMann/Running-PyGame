b17ac9fe501e1b4de250073773bd0088a74ec624
Sethabramowitz <sethabramowitz19@sasaustin.org>
2019-03-13 13:51:14 -0500

    updated code

--- bigTimeTim.py
+++ bigTimeTim.py
@@ -1,22 +1,28 @@
 import pygame
+import sys
+import os
 
 pygame.init()
 screen = pygame.display.set_mode((640, 640))
 done = False
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self,image,image2,x,y):s
-        super().__init__()
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.movex = 0
+        self.movey = 0
+        self.frame = 0
+        self.health = 1
+        self.score = 1
+        self.images = []
+        for i in range(1,9):
+            img = pygame.image.load(os.path.join('images','hero' + str(i) + '.png')).convert()
+            img.convert_alpha()
+            img.set_colorkey(ALPHA)
+            self.images.append(img)
+            self.image = self.images[0]
+            self.rect  = self.image.get_rect()
 
-        self.image = pygame.image.load('img1.png')
-        self.image2 = pygame.image.load('img2.png')
-        self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
-        self.jumping = False
-
-	def jump(self):
-        pass
 
 class Platform(pygame.sprite.Sprite):
     def __init__(self,color,x,y,height,width):
@@ -25,7 +31,8 @@ class Platform(pygame.sprite.Sprite):
         self.image.fill(color)
         self.rect = self.image.get_rect()
         self.rect.x = x
-        self.rect.y = y            
+        self.rect.y = y
+
 while not done:
         for event in pygame.event.get():
                 if event.type == pygame.QUIT:
